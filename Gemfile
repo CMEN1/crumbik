2d8b4e7e0155ce169a7560589ab7cf7399fdb1b0
Enmanuel <enmanuelm19@gmail.com>
2015-07-14 10:44:51 -0430

    Agregada funcionalidad para loguearse con google

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '4.2.0'
 gem 'mysql2'
 gem 'devise'
 gem 'materialize-sass'
+gem 'omniauth-google-oauth2'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,49 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauth_providers => [:google_oauth2]
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource = nil)
+
+    data = access_token.info
+
+    user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+
+    if user
+
+      return user
+
+    else
+
+      registered_user = User.where(:email => access_token.info.email).first
+
+      if registered_user
+
+        return registered_user
+
+      else
+
+        access_token.provider = “Google”
+
+        user = User.create(first_name: data[“first_name”],
+
+            last_name: data[“last_name”],
+
+            provider:access_token.provider,
+
+            email: data[“email”],
+
+            password: Devise.friendly_token[0,20],
+
+            confirmed_at:Time.zone.now # if u don’t want to send any confirmation mail
+
+        )
+
+      end
+
+    end
+
+  end
+
 end
